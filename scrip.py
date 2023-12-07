e46b9ac4512b702bfac87d75cf28d12cf59b5c75
Arlen <a870128rlen@gmail.com>
2023-12-07 20:08:44 +0800

    fix: time limited

--- scrip.py
+++ scrip.py
@@ -1,32 +1,56 @@
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 import sounddevice as sd
+import soundfile as sf
 import numpy as np
 
 
-# 錄製參數
-duration = 45  # 錄製10秒音頻
-sampling_rate = 16000  # 採樣率
+# parameters
+duration = 45  # total recording duration
+sampling_rate = 16000  # sampling rate of audio
+segment_length = 10  # length of each segment in seconds
 
-# 錄製音頻
-print("開始錄音...")
-audio = sd.rec(int(duration * sampling_rate), samplerate=sampling_rate, channels=1)
-sd.wait()
-print("錄音結束.")
+def record(duration, sampling_rate):
+    print("開始錄音...")
+    audio = sd.rec(int(duration * sampling_rate), samplerate=sampling_rate, channels=1)
+    sd.wait()
+    sf.write("recording.wav", audio, sampling_rate)
+    print("錄音結束.")
+    return audio
 
-# 將音頻轉換為numpy數組
-audio_array = np.squeeze(audio)
+def transcribe(audio):
+    # split audio into segments
+    audio_array = np.squeeze(audio)
+    segments = np.array_split(audio_array, np.ceil(duration / segment_length))
 
-# load model and processor
-processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2")
-model.config.forced_decoder_ids = None
+    # load model and processor
+    processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2")
+    model.config.forced_decoder_ids = None
 
-input_features = processor(audio_array, sampling_rate=sampling_rate, return_tensors="pt").input_features 
+    transcriptions = []
+    for segment in segments:
+        input_features = processor(segment, sampling_rate=sampling_rate, return_tensors="pt").input_features
+        predicted_ids = model.generate(input_features)
+        transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+        transcriptions.append(transcription[0])
+    return transcriptions
 
-# generate token ids
-predicted_ids = model.generate(input_features)
-# decode token ids to text
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
-print(transcription)
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-print(transcription)
+def clean_transcription(transcriptions):
+    cleaned_transcriptions = [sentence.strip() for sentence in transcriptions]
+    full_text = " ".join(cleaned_transcriptions)
+    return full_text
+
+
+def main():
+    audio = record(duration, sampling_rate)
+
+    # transcribe audio
+    if audio is not None:
+        transcriptions = transcribe(audio)
+        full_text = clean_transcription(transcriptions)
+        print(full_text)
+    else:
+        print("No audio was recorded.")
+
+if __name__ == "__main__":
+    main()
